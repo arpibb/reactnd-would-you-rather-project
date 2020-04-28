41121822243c73fbee10f435c4279870de91e822
Arpad Bencze <bencze.arpad.21@gmail.com>
2020-04-28 20:09:52 +0200

    adding file structure

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,3 +4,22 @@ export function formatDate (timestamp) {
   return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
 }
 
+export function formatQuestion (tweet, author, authedUser, parentTweet) {
+  const { id, likes, replies, text, timestamp } = tweet
+  const { name, avatarURL } = author
+
+  return {
+    name,
+    id,
+    timestamp,
+    text,
+    avatar: avatarURL,
+    likes: likes.length,
+    replies: replies.length,
+    hasLiked: likes.includes(authedUser),
+    parent: !parentTweet ? null : {
+      author: parentTweet.author,
+      id: parentTweet.id,
+    }
+  }
+}
