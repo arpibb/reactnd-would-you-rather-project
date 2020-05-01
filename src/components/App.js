4d6a47d1fe1206e7a4c6bdee415d3fa7394f476b
Arpad Bencze <bencze.arpad.21@gmail.com>
2020-05-01 11:54:55 +0200

    store setup and basic receive actions

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Login from './Login'
-import { receiveUsers } from '../actions/users'
-
+import { handleInitialData } from '../actions/shared'
 
 
 class App extends Component {
   componentDidMount(){
-    this.props.dispatch(receiveUsers())
+    this.props.dispatch(handleInitialData())
   }
   render(){
+    const { authedUser } = this.props
+    //console.log(authedUser)
     return (
       <div className="App">
         <header className="App-header">
@@ -20,9 +21,9 @@ class App extends Component {
   } 
 }
 
-function mapStateToProps({users}){
+function mapStateToProps({authedUser}){
   return {
-    loading: users === undefined
+    loading: authedUser === null
   }
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-
 class Login extends Component {
   render(){
     return(
