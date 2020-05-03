5a1966c6e487a2150c3c38070626bff0584ccfd3
Arpad Bencze <bencze.arpad.21@gmail.com>
2020-05-03 16:17:48 +0200

    leaderboard user card adding css

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import '../styles/App.scss'
 
 class UserCard extends Component{
   render(){
@@ -10,14 +11,15 @@ class UserCard extends Component{
     return(
       <div className="usercard">
         <div className="avatar-container">
-          <img src={avatarURL} alt={`Avatar of ${name}`}/>
+          <img className="avatar-big" src={avatarURL} alt={`Avatar of ${name}`}/>
         </div>
-        <div>
-          <h3>{name}</h3>
-          <span>Answered questions</span><span>{answeredQuestionsNum}</span>
-          <span>Created questions</span><span>{createdQuestionsNum}</span>
+        <div className="description-container">
+          <h2>{name}</h2>
+          <p><span>Answered questions</span><span className="right-span-score">{answeredQuestionsNum}</span></p>
+          <hr/>
+          <p><span>Created questions</span><span className="right-span-score">{createdQuestionsNum}</span></p>
         </div>
-        <div>
+        <div className="score-container">
           <div>
             <h4>Score</h4>
             <h3>{answeredQuestionsNum + createdQuestionsNum}</h3>
