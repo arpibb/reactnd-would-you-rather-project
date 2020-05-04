788965609c8e6e73ef14e38c738ac23faaedce67
Arpad Bencze <bencze.arpad.21@gmail.com>
2020-05-04 20:40:11 +0200

    starting out with Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,44 @@ import { connect } from 'react-redux'
 import '../styles/App.scss'
 
 class Home extends Component{
+  state = {
+    answered: false,
+  }
+
+  handleFlip = () => {
+    this.setState((prevState)=>({
+      answered: !prevState.answered
+    }))
+  }
+
+
   render(){
+    const { answered } = this.state
+    const { authedUser, users, questions } = this.props
     return(
-      <div>This is the Homepage</div>
+      <div className="home-container">
+        <div className="buttons-container">
+          <div className="answer-btn">
+            <p>Unanswered Questions</p>
+          </div>
+          <div className="answer-btn">
+            <p>Answered Questions</p>
+          </div>
+        </div>
+        <div className="answers-container">
+          
+        </div>
+      </div>
     )
   }
 }
 
-export default connect()(Home)
+function mapStateToProps({authedUser,users,questions}){
+  return {
+    authedUser,
+    users,
+    questions
+  }
+}
+
+export default connect(mapStateToProps)(Home)
