667c683e6698aec3f02a8c9bf8b4ef93344851ff
Arpad Bencze <bencze.arpad.21@gmail.com>
2020-05-06 22:48:54 +0200

    sorting users on leaderboard based on points

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -3,11 +3,16 @@ import { connect } from 'react-redux'
 import UserCard from './UserCard'
 
 class Leaderboard extends Component{
+
+  sortUsers = (a,b) => {
+    return (Object.keys(b.answers).length + b.questions.length) - (Object.keys(a.answers).length + a.questions.length)
+  }
+
   render(){
     const { users } = this.props
     return(
       <div>
-        {users && users.map((user)=>{
+        {users && users.sort((a,b) => this.sortUsers(a,b)).map((user)=>{
           return(
             <UserCard key={user.id} id={user.id} />
           )
